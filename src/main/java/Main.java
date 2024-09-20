c34c9817299de9c1ce55e66cd9583b2f77b7ac7f
bellaisgroovy <blockyelephant@gmail.com>
2024-09-20 09:42:07 +0100

    improved readability

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,21 @@
 import game.Game;
-import game.SimpleGame;
 import game.gameFactory.GameFactory;
 import game.gameFactory.SimpleGameFactory;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
     static GameFactory gameFactory = new SimpleGameFactory();
     public static void main(String[] args) {
+        // create int list from string in format 1,2,3,4
         List<Integer> boardSizes = Arrays.stream(args[0].split(",")).map(s -> Integer.parseInt(s.trim())).toList();
-        Game game = gameFactory.createGame(boardSizes, Integer.parseInt(args[1]), Integer.parseInt(args[2]));
 
+        Game game = gameFactory.createGame(
+                boardSizes,
+                Integer.parseInt(args[1]),
+                Integer.parseInt(args[2])
+        );
         System.out.println(game.play());
     }
 }

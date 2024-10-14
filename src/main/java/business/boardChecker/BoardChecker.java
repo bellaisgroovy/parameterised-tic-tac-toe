ebb3153b7a18c7033a9dbec22db9430e92a01015
bellaisgroovy <blockyelephant@gmail.com>
2024-10-14 16:25:04 +0100

    added javadoc

--- src/main/java/business/boardChecker/BoardChecker.java
+++ src/main/java/business/boardChecker/BoardChecker.java
@@ -3,5 +3,11 @@ package business.boardChecker;
 import business.data.Board;
 
 public interface BoardChecker {
+    /**
+     * checks which player, if any, has won
+     * @param board
+     * a Board object
+     * @return an int representing game state, -1 = ongoing, 0 = draw, positive integer = the player who won
+     */
     int winningPlayer(Board board);
 }

650aa867c5211a83b1f34b37f63a09d2d5d71fb6
bellaisgroovy <blockyelephant@gmail.com>
2024-10-14 16:48:58 +0100

    optimized imports

--- src/test/java/data/FileBoardSaveReaderTest.java
+++ src/test/java/data/FileBoardSaveReaderTest.java
@@ -2,7 +2,6 @@ package data;
 
 import business.data.Board;
 import business.data.SimpleBoard;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 

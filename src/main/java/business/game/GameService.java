2092e073d4e306240887bcab823e37210b2949b5
bellaisgroovy <blockyelephant@gmail.com>
2024-10-14 16:44:42 +0100

    added javadoc and changed contract

--- src/main/java/business/game/GameService.java
+++ src/main/java/business/game/GameService.java
@@ -5,7 +5,32 @@ import business.data.Board;
 import java.util.List;
 
 public interface GameService {
-    Board getBoard();
+    /**
+     * gets current state of the specified game
+     * @param gameName
+     * name of game to retrieve
+     * @return current state of board
+     */
+    Board getBoard(String gameName);
 
-    void playInCell(List<Integer> coordinate, int player);
+    /**
+     * sets cell at cellCoordinate to player in game of name gameName
+     * @param cellCoordinate
+     * coordinates in each dimension of board playing in
+     * @param player
+     * int representing player playing
+     * @param gameName
+     * name of game to play on
+     * @return
+     * true if move is valid, false/error if not
+     */
+    boolean playInCell(List<Integer> cellCoordinate, int player, String gameName);
+
+    /**
+     * checks which player, if any, has won in specified game
+     * @param gameName
+     * name of a game
+     * @return an int representing game state, -1 = ongoing, 0 = draw, positive integer = the player who won
+     */
+    int getWinner(String gameName);
 }

7fecc09f4eccd4d3520ad57cf7ca9c70efd0e5da
bellaisgroovy <blockyelephant@gmail.com>
2024-10-14 22:05:25 +0100

    added docstrings

--- src/main/java/data/BoardSaveWriter.java
+++ src/main/java/data/BoardSaveWriter.java
@@ -3,5 +3,11 @@ package data;
 import game.data.Board;
 
 public interface BoardSaveWriter {
+    /**
+     * @param board
+     * board to be saved
+     * @param saveName
+     * name given
+     */
     void saveBoard(Board board, String saveName);
 }

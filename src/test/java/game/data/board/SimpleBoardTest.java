46f4ee7bccbca693ce510b1b8eab0afddb401304
bellaisgroovy <blockyelephant@gmail.com>
2024-10-20 17:23:06 +0100

    removed unused import

--- src/test/java/game/data/board/SimpleBoardTest.java
+++ src/test/java/game/data/board/SimpleBoardTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
